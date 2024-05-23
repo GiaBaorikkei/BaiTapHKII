c3f628816057cf5e41605aa12bbd1b8a028a7908
GiaBaorikkei <thainguyengiabao082105@gmail.com>
2024-05-23 11:37:35 +0700

    update

---
+++ session41/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Baitap1 from './components/Bài1/Baitap1'
+import Baitap2 from './components/Bài2/Baitap2'
+import Baitap3 from './components/Bài3/Baitap3'
+import Baitap4 from './components/Bài4/Baitap4'
+import Baitap5 from './components/Bài5/Baitap5'
+import Baitap6 from './components/Bài6/Baitap6'
+import Baitap7 from './components/Bài7/Baitap7'
+import Baitap8 from './components/Bài8/Baitap8'
+import Baitap9 from './components/Bài9/Baitap9'
+import Baitap10 from './components/Bài10/Baitap10'
+
+export default function App() {
+  return (
+    <div>App
+      <Baitap1></Baitap1>
+      <Baitap2></Baitap2>
+      <Baitap3></Baitap3>
+      <Baitap4></Baitap4>
+      <Baitap5></Baitap5>
+      <Baitap6></Baitap6>
+      <Baitap7></Baitap7>
+      <Baitap8></Baitap8>
+      <Baitap9></Baitap9>
+      <Baitap10></Baitap10>
+    </div>
+  )
+}
---
+++ session41/src/components/Bài1/Baitap1.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Baitap1() {
+  return (
+    <div>
+        <h1 className='text-red-500 text-2xl'>Thái Nguyễn Gia Bảo</h1>
+    </div>
+  )
+}
