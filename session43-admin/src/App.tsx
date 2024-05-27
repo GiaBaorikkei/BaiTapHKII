cf206b6d68f327c43d28f226a45ad3b8258c307d
GiaBaorikkei <thainguyengiabao082105@gmail.com>
2024-05-27 13:27:39 +0700

    update

--- session43-admin/src/App.tsx
+++ session43-admin/src/App.tsx
@@ -1,9 +1,10 @@
-
+import Admin from "./components/Admin";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <div>App
-        Admin
+        <Admin></Admin>
     </div>
   )
 }
